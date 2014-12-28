4cdebe5212e713891894e5de798db4fffc71003b
kutiman <kutiman@gmail.com>
2014-12-29 04:14:49 +0530

    starting out

---
+++ scrCube.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var cubeLevel : int = 0;
+var textObject : GameObject;
+
+function Start () {
+	CreateText();
+	SetText();
+}
+
+function Update () {
+
+}
+
+function CreateText () {
+	textObject = GameObject.Instantiate(Resources.Load("Prefabs/objText"));
+	textObject.transform.parent = gameObject.transform;
+	textObject.transform.localPosition = Vector3(0,0,0);
+	textObject.transform.localScale = Vector3(1,1,1);
+	textObject.GetComponent(TextMesh).text = "";
+}
+
+function SetText () {
+	var textToShow = "";
+	if (cubeLevel > 0) {
+		textToShow = (cubeLevel).ToString();
+	}
+	textObject.GetComponent(TextMesh).text = textToShow;
+}
+
+function OnMouseDown () {
+	cubeLevel++;
+	SetText();
+}
---
+++ scrGame.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var fieldArray : int[,];
+var objField : GameObject;
+
+function Start () {
+	fieldArray = CreateNewField();
+	CreateCubes(fieldArray);
+}
+
+function Update () {
+
+}
+
+function CreateNewField () {
+	var arr : int[,] = new int[5,5];
+	for (var i = 0; i < 5; i++) {
+		for (var n = 0; n < 5; n++){
+			arr[i,n] = 0;
+		}
+	}
+	return arr;
+}
+
+function CreateCubes (arr : int[,]) {
+	for (var i = 0; i < 5; i++) {
+		for (var n = 0; n < 5; n++){
+			var cubeLvl = arr[i,n];
+			var obj : GameObject = GameObject.Instantiate(Resources.Load("Prefabs/Cube"));
+			var ancX = -4.8;
+			var ancY = -4.8;
+			obj.transform.parent = GameObject.Find("objField").transform;
+			obj.transform.localPosition = Vector3(ancX + i * 2.4, ancY + n * 2.4, -1);
+			//obj.transform.localScale = Vector3(1,1,1);
+		}
+	}
+}
